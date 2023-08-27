52b5d2e501faac92e04dddfbd47a8f16a05d73ae
niyatijain1 <118036643+niyatijain1@users.noreply.github.com>
2023-08-27 23:53:50 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (username === '') {
+      return;
+    }
+
+    try {
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      const userData = await response.json();
+
+      if (userData.message === 'Not Found') {
+        setError('User not found');
+        setUserData(null);
+        return;
+      }
+
+      setUserData(userData);
+      setError(null);
+
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      setError('An error occurred while fetching user data');
+      setUserData(null);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Github User Info..</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={username}
+            onChange={handleChange}
+            placeholder="Enter GitHub Username"
+            className="input-field"
+            required
+          />
+          <button type="submit" className="submit-button">Get Info</button>
+        </form>
+        {error && <p className="error">{error}</p>}
+        {userData && (
+          <div className="card">
+            <img src={userData.avatar_url} alt="User Avatar" className="avatar" />
+            <h2 className="username">{userData.login}</h2>
+            <p className="name">Name: {userData.name || 'N/A'}</p>
+            <p className="repos">Public Repos: {userData.public_repos}</p>
+            <p className="gists">Public Gists: {userData.public_gists}</p>
+            <p className="created-at">Profile Created At: {new Date(userData.created_at).toLocaleDateString()}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
 
 export default App;
+
